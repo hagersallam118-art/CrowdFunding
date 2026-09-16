680675ef1aa427c652a42aaad05c811405c02efa
Hager <hagersallam118@gmail.com>
2026-09-16 04:45:07 +0300

    Update email settings and database

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = "django-insecure-change-this-in-production"
 DEBUG = True
 ALLOWED_HOSTS = []
@@ -89,8 +90,15 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "no-reply@crowdfunding.local"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "profile"
